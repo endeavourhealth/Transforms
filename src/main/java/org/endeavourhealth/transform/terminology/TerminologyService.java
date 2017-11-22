9f1aec5115ebdd2256915ace519c15ac8dc7b27e
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2017-11-22 11:48:35 +0000

    Updated to include CTV3 to Snomed map DAL implementation

--- src/main/java/org/endeavourhealth/transform/terminology/TerminologyService.java
+++ src/main/java/org/endeavourhealth/transform/terminology/TerminologyService.java
@@ -3,8 +3,10 @@ package org.endeavourhealth.transform.terminology;
 import com.google.common.base.Strings;
 import org.endeavourhealth.common.fhir.FhirUri;
 import org.endeavourhealth.core.database.dal.DalProvider;
+import org.endeavourhealth.core.database.dal.reference.CTV3ToSnomedMapDalI;
 import org.endeavourhealth.core.database.dal.reference.Read2ToSnomedMapDalI;
 import org.endeavourhealth.core.database.dal.reference.SnomedDalI;
+import org.endeavourhealth.core.database.dal.reference.models.CTV3ToSnomedMap;
 import org.endeavourhealth.core.database.dal.reference.models.Read2ToSnomedMap;
 import org.endeavourhealth.core.database.dal.reference.models.SnomedLookup;
 import org.endeavourhealth.transform.common.exceptions.TransformException;
@@ -17,6 +19,7 @@ public abstract class TerminologyService {
 
     private static SnomedDalI snomedRepository = DalProvider.factorySnomedDal();
     private static Read2ToSnomedMapDalI read2ToSnomedRepository = DalProvider.factoryRead2ToSnomedMapDal();
+    private static CTV3ToSnomedMapDalI ctv3ToSnomedRepository = DalProvider.factoryCTV3ToSnomedMapDal();
 
     public static SnomedCode lookupSnomedFromConceptId(String conceptId) throws Exception {
         SnomedLookup snomedLookup = snomedRepository.getSnomedLookup(conceptId);
@@ -25,18 +28,23 @@ public abstract class TerminologyService {
     }
 
     public static SnomedCode translateRead2ToSnomed(String code) throws Exception {
-        //1)  get conceptId from Read2/Snomed map table
+        //get conceptId from Read2/Snomed map table
         Read2ToSnomedMap read2ToSnomedMap = read2ToSnomedRepository.getRead2ToSnomedMap(code);
-
-        //2) get Snomed term from lookup table
         String conceptId = read2ToSnomedMap.getConceptId();
-        SnomedLookup snomedLookup = snomedRepository.getSnomedLookup(conceptId);
 
+        //get Snomed term from lookup table using conceptId
+        SnomedLookup snomedLookup = snomedRepository.getSnomedLookup(conceptId);
         return new SnomedCode(conceptId, snomedLookup.getTerm());
     }
-    public static SnomedCode translateCtv3ToSnomed(String code) {
-        //TODO - terminology service needs completing
-        return null;
+
+    public static SnomedCode translateCtv3ToSnomed(String code) throws Exception {
+        //get conceptId from CTV3/Snomed map table
+        CTV3ToSnomedMap ctv3ToSnomedMap = ctv3ToSnomedRepository.getCTV3ToSnomedMap(code);
+        String sctConceptId = ctv3ToSnomedMap.getSCTConceptId();
+
+        //get Snomed term from lookup table using conceptId
+        SnomedLookup snomedLookup = snomedRepository.getSnomedLookup(sctConceptId);
+        return new SnomedCode(sctConceptId, snomedLookup.getTerm());
     }
     public static SnomedCode translateEmisSnomedToSnomed(String code) {
         //TODO - terminology service needs completing
