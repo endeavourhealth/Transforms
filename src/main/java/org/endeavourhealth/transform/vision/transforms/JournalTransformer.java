1283aa39837501b0508df145a20e188dc36ad913
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2017-12-12 09:20:38 +0000

    Fix to Vision Procedure codes where numeric entries exist, so treat as an Observation

--- src/main/java/org/endeavourhealth/transform/vision/transforms/JournalTransformer.java
+++ src/main/java/org/endeavourhealth/transform/vision/transforms/JournalTransformer.java
@@ -210,7 +210,7 @@ public class JournalTransformer {
             L = Allergy
         */
         String readCode = parser.getReadCode();
-        if (Read2.isProcedure(readCode) && !Read2.isBPCode(readCode)) {
+        if (Read2.isProcedure(readCode) && !Read2.isBPCode(readCode) && !subset.equalsIgnoreCase("T")) {
             return ResourceType.Procedure;
         } else if (Read2.isDisorder(readCode) || subset.equalsIgnoreCase("P")) {
             return ResourceType.Condition;
