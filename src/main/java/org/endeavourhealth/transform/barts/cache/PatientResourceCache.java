b1826027a6532ce25a074a7b07c269c4b52b9a9d
stuart <stuart@bearclawsolutions.co.uk>
2018-02-16 12:31:34 +0000

    Add a function to save all the patient resources in one go

--- src/main/java/org/endeavourhealth/transform/barts/cache/PatientResourceCache.java
+++ src/main/java/org/endeavourhealth/transform/barts/cache/PatientResourceCache.java
@@ -1,10 +1,14 @@
 package org.endeavourhealth.transform.barts.cache;
 
+import org.endeavourhealth.transform.common.BasisTransformer;
+import org.endeavourhealth.transform.common.CsvCurrentState;
+import org.endeavourhealth.transform.common.FhirResourceFiler;
 import org.hl7.fhir.instance.model.Patient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class PatientResourceCache {
@@ -19,5 +23,20 @@ public class PatientResourceCache {
 
     public static Patient getPatientResource(Long millenniumId) throws Exception {
         return patientResources.get(millenniumId);
+    }
+
+    private static void filePatientResources(FhirResourceFiler fhirResourceFiler) throws Exception {
+
+        CsvCurrentState parserState = new CsvCurrentState("PPATI", 0);
+
+        Iterator<Map.Entry<Long,Patient>> iter = patientResources.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry<Long,Patient> entry = iter.next();
+            Patient fhirPatient = entry.getValue();
+            BasisTransformer.savePatientResource(fhirResourceFiler, parserState, fhirPatient.getId().toString(), fhirPatient);
+                iter.remove();
+
+        }
+
     }
 }
