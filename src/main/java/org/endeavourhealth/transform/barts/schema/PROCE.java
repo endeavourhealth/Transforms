b66dd2eb103ace41fd3cc33caf319436643438f4
stuart <stuart@bearclawsolutions.co.uk>
2018-02-20 09:34:34 +0000

    Change the PROCE files to use the AbtractCsvMethod

--- src/main/java/org/endeavourhealth/transform/barts/schema/PROCE.java
+++ src/main/java/org/endeavourhealth/transform/barts/schema/PROCE.java
@@ -1,52 +1,56 @@
 package org.endeavourhealth.transform.barts.schema;
 
 import org.endeavourhealth.core.exceptions.TransformException;
-import org.endeavourhealth.transform.barts.AbstractCharacterParser;
-import org.endeavourhealth.transform.common.exceptions.FileFormatException;
+import org.endeavourhealth.transform.barts.BartsCsvToFhirTransformer;
+import org.endeavourhealth.transform.common.AbstractCsvParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 import java.util.UUID;
 
-public class PROCE extends AbstractCharacterParser {
+public class PROCE extends AbstractCsvParser {
     private static final Logger LOG = LoggerFactory.getLogger(PROCE.class);
 
-    public static final String DATE_FORMAT = "dd/mm/yyyy";
-    public static final String TIME_FORMAT = "hh:mm:ss";
-    public static final String DATE_TIME_FORMAT = DATE_FORMAT + " " + TIME_FORMAT;
-
     public PROCE(UUID serviceId, UUID systemId, UUID exchangeId, String version, String filePath, boolean openParser) throws Exception {
-        super(serviceId, systemId, exchangeId, version, filePath, "\\|", openParser, DATE_FORMAT, TIME_FORMAT);
-
-        addFieldList("MillenniumProcedureId");
-        addFieldList("ActiveIndicator");
-        addFieldList("ExtractDateTime");
-        addFieldList("MillenniumEncounterIdentifier");
-        addFieldList("MillenniumEncounterSliceIdentifier");
-        addFieldList("CDSSequence");
-        addFieldList("MillenniumNomenclatureIdentifier");
-        addFieldList("ProcedureDateTime");
-        addFieldList("ProcedureMillenniumPersonnelIdentifier");
-        addFieldList("ProcedureTypeMillenniumCode");
-        addFieldList("MillenniumConceptIdentifier");
-        addFieldList("ProcedureCodeSequenceEntryOrder");
+        super(serviceId, systemId, exchangeId, version, filePath, openParser,
+                BartsCsvToFhirTransformer.CSV_FORMAT,
+                BartsCsvToFhirTransformer.DATE_FORMAT_YYYY_MM_DD,
+                BartsCsvToFhirTransformer.TIME_FORMAT);
     }
 
-    public String getProcedureID() throws FileFormatException {
-        return super.getString("MillenniumProcedureId");
+    @Override
+    protected String[] getCsvHeaders(String version) {
+        return new String[] {
+                "PROCEDURE_ID",
+                "ACTIVE_IND",
+                "EXTRACT_DT_TM",
+                "ENCNTR_ID",
+                "ENCNTR_SLICE_ID",
+                "PROCEDURE_SEQ_NBR",
+                "NOMENCLATURE_ID",
+                "PROCEDURE_DT_TM",
+                "PROCEDURE_HCP_PRSNL_ID",
+                "PROCEDURE_TYPE_CD",
+                "CONCEPT_CKI_IDENT",
+                "PROCEDURE_ENTRY_SEQ_NBR"
+        };
     }
 
-    public int getActiveIndicator() throws FileFormatException {
-        return super.getInt("ActiveIndicator");
+    public String getProcedureID() {
+        return super.getString("PROCEDURE_ID");
+    }
+
+    public int getActiveIndicator() {
+        return super.getInt("ACTIVE_IND");
     }
 
     public Date getExtractDateTime() throws TransformException {
-        return super.getDateTime("ExtractDateTime");
+        return super.getDate("EXTRACT_DT_TM");
     }
 
-    public boolean isActive() throws FileFormatException {
-        int val = super.getInt("ActiveIndicator");
+    public boolean isActive() {
+        int val = super.getInt("ACTIVE_IND");
         if (val == 1) {
             return true;
         } else {
@@ -54,44 +58,44 @@ public class PROCE extends AbstractCharacterParser {
         }
     }
 
-    public String getEncounterID() throws FileFormatException {
-        return super.getString("MillenniumEncounterIdentifier");
+    public String getEncounterID() {
+        return super.getString("ENCNTR_ID");
     }
 
-    public String getEncounterSliceID() throws FileFormatException {
-        return super.getString("MillenniumEncounterSliceIdentifier");
+    public String getEncounterSliceID() {
+        return super.getString("ENCNTR_SLICE_ID");
     }
 
-    public String getCDSSequence() throws FileFormatException {
-        return super.getString("CDSSequence");
+    public String getCDSSequence() {
+        return super.getString("PROCEDURE_SEQ_NBR");
     }
 
-    public String getNomenclatureID() throws FileFormatException {
-        return super.getString("MillenniumNomenclatureIdentifier");
+    public String getNomenclatureID() {
+        return super.getString("NOMENCLATURE_ID");
     }
 
     public Date getProcedureDateTime() throws TransformException {
-        return super.getDate("ProcedureDateTime");
+        return super.getDate("");
     }
 
-    public String getProcedureDateTimeAsString() throws TransformException {
-        return super.getString("ProcedureDateTime");
+    public String getProcedureDateTimeAsString() {
+        return super.getString("PROCEDURE_DT_TM");
     }
 
-    public String getPersonnelID() throws FileFormatException {
-        return super.getString("ProcedureMillenniumPersonnelIdentifier");
+    public String getPersonnelID() {
+        return super.getString("PROCEDURE_HCP_PRSNL_ID");
     }
 
-    public Long getProcedureTypeCode() throws FileFormatException {
-        return super.getLong("ProcedureTypeMillenniumCode");
+    public Long getProcedureTypeCode() {
+        return super.getLong("PROCEDURE_TYPE_CD");
     }
 
-    public String getConceptCodeIdentifier() throws FileFormatException {
-        return super.getString("MillenniumConceptIdentifier");
+    public String getConceptCodeIdentifier() {
+        return super.getString("CONCEPT_CKI_IDENT");
     }
 
-    public String getConceptCodeType() throws FileFormatException {
-        String conceptCodeIdentifier = super.getString("MillenniumConceptIdentifier");
+    public String getConceptCodeType() {
+        String conceptCodeIdentifier = super.getString("CONCEPT_CKI_IDENT");
         int index = conceptCodeIdentifier.indexOf('!');
         if (index > -1) {
             return conceptCodeIdentifier.substring(0,index);
@@ -100,8 +104,8 @@ public class PROCE extends AbstractCharacterParser {
         }
     }
 
-    public String getConceptCode() throws FileFormatException {
-        String conceptCodeIdentifier = super.getString("MillenniumConceptIdentifier");
+    public String getConceptCode() {
+        String conceptCodeIdentifier = super.getString("CONCEPT_CKI_IDENT");
         int index = conceptCodeIdentifier.indexOf('!');
         if (index > -1) {
             return conceptCodeIdentifier.substring(index + 1);
@@ -110,12 +114,17 @@ public class PROCE extends AbstractCharacterParser {
         }
     }
 
-    public String geProcedureCodeSequenceEntryOrder() throws FileFormatException {
-        return super.getString("ProcedureCodeSequenceEntryOrder");
+    public String geProcedureCodeSequenceEntryOrder() {
+        return super.getString("PROCEDURE_ENTRY_SEQ_NBR");
     }
 
     @Override
     protected String getFileTypeDescription() {
         return "Cerner person relationship file";
+    }
+
+    @Override
+    protected boolean isFileAudited() {
+        return true;
     }
 }
