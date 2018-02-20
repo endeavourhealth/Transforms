6df7c57dfd19f048fa702aa928d6a984f0ae3193
stuart <stuart@bearclawsolutions.co.uk>
2018-02-20 09:27:24 +0000

    Change the DIAGN files to use the AbtractCsvMethod

--- src/main/java/org/endeavourhealth/transform/barts/schema/DIAGN.java
+++ src/main/java/org/endeavourhealth/transform/barts/schema/DIAGN.java
@@ -1,53 +1,57 @@
 package org.endeavourhealth.transform.barts.schema;
 
 import org.endeavourhealth.core.exceptions.TransformException;
-import org.endeavourhealth.transform.barts.AbstractCharacterParser;
-import org.endeavourhealth.transform.common.exceptions.FileFormatException;
+import org.endeavourhealth.transform.barts.BartsCsvToFhirTransformer;
+import org.endeavourhealth.transform.common.AbstractCsvParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 import java.util.UUID;
 
-public class DIAGN extends AbstractCharacterParser {
+public class DIAGN extends AbstractCsvParser {
     private static final Logger LOG = LoggerFactory.getLogger(DIAGN.class);
 
-    public static final String DATE_FORMAT = "dd/mm/yyyy";
-    public static final String TIME_FORMAT = "hh:mm:ss";
-    public static final String DATE_TIME_FORMAT = DATE_FORMAT + " " + TIME_FORMAT;
-
     public DIAGN(UUID serviceId, UUID systemId, UUID exchangeId, String version, String filePath, boolean openParser) throws Exception {
-        super(serviceId, systemId, exchangeId, version, filePath, "\\|", openParser, DATE_FORMAT, TIME_FORMAT);
-
-        addFieldList("MillenniumDiagnosisId");
-        addFieldList("ActiveIndicator");
-        addFieldList("ExtractDateTime");
-        addFieldList("MillenniumEncounterIdentifier");
-        addFieldList("MillenniumEncounterSliceIdentifier");
-        addFieldList("CDSSequence");
-        addFieldList("MillenniumNomenclatureIdentifier");
-        addFieldList("DiagnosisDateTime");
-        addFieldList("DiagnosisMillenniumPersonnelIdentifier");
-        addFieldList("DiagnosisTypeMillenniumCode");
-        addFieldList("MillenniumConceptIdentifier");
-        addFieldList("DiagnosisFreeText");
-        addFieldList("DiagnosisCodeSequenceEntryOrder");
+        super(serviceId, systemId, exchangeId, version, filePath, openParser,
+                BartsCsvToFhirTransformer.CSV_FORMAT,
+                BartsCsvToFhirTransformer.DATE_FORMAT_YYYY_MM_DD,
+                BartsCsvToFhirTransformer.TIME_FORMAT);
     }
 
-    public String getDiagnosisID() throws FileFormatException {
-        return super.getString("MillenniumDiagnosisId");
+    @Override
+    protected String[] getCsvHeaders(String version) {
+        return new String[]{
+                "DIAGNOSIS_ID",
+                "ACTIVE_IND",
+                "EXTRACT_DT_TM",
+                "ENCNTR_ID",
+                "ENCNTR_SLICE_ID",
+                "DIAGNOSIS_SEQ_NBR",
+                "NOMENCLATURE_ID",
+                "DIAGNOSIS_DT_TM",
+                "DIAG_HCP_PRSNL_ID",
+                "DIAGNOSIS_TYPE_CD",
+                "CONCEPT_CKI_IDENT",
+                "DIAGNOSIS_TXT",
+                "DIAGNOSIS_ENTRY_SEQ_NBR",
+        };
     }
 
-    public int getActiveIndicator() throws FileFormatException {
-        return super.getInt("ActiveIndicator");
+    public String getDiagnosisID() {
+        return super.getString("DIAGNOSIS_ID");
+    }
+
+    public int getActiveIndicator() {
+        return super.getInt("ACTIVE_IND");
     }
 
     public Date getExtractDateTime() throws TransformException {
-        return super.getDateTime("ExtractDateTime");
+        return super.getDate("EXTRACT_DT_TM");
     }
 
-    public boolean isActive() throws FileFormatException {
-        int val = super.getInt("ActiveIndicator");
+    public boolean isActive() {
+        int val = super.getInt("ACTIVE_IND");
         if (val == 1) {
             return true;
         } else {
@@ -55,44 +59,44 @@ public class DIAGN extends AbstractCharacterParser {
         }
     }
 
-    public String getEncounterID() throws FileFormatException {
-        return super.getString("MillenniumEncounterIdentifier");
+    public String getEncounterID() {
+        return super.getString("ENCNTR_ID");
     }
 
-    public String getEncounterSliceID() throws FileFormatException {
-        return super.getString("MillenniumEncounterSliceIdentifier");
+    public String getEncounterSliceID() {
+        return super.getString("ENCNTR_SLICE_ID");
     }
 
-    public String getCDSSequence() throws FileFormatException {
-        return super.getString("CDSSequence");
+    public String getCDSSequence() {
+        return super.getString("DIAGNOSIS_SEQ_NBR");
     }
 
-    public String getNomenclatureID() throws FileFormatException {
-        return super.getString("MillenniumNomenclatureIdentifier");
+    public String getNomenclatureID() {
+        return super.getString("NOMENCLATURE_ID");
     }
 
     public Date getDiagnosisDateTime() throws TransformException {
-        return super.getDate("DiagnosisDateTime");
+        return super.getDate("DIAGNOSIS_DT_TM");
     }
 
-    public String getDiagnosisDateTimeAsString() throws TransformException {
-        return super.getString("DiagnosisDateTime");
+    public String getDiagnosisDateTimeAsString() {
+        return super.getString("DIAGNOSIS_DT_TM");
     }
 
-    public String getPersonnel() throws FileFormatException {
-        return super.getString("DiagnosisMillenniumPersonnelIdentifier");
+    public String getPersonnel() {
+        return super.getString("DIAG_HCP_PRSNL_ID");
     }
 
-    public Long getDiagnosisTypeCode() throws FileFormatException {
-        return super.getLong("DiagnosisTypeMillenniumCode");
+    public Long getDiagnosisTypeCode() {
+        return super.getLong("DIAGNOSIS_TYPE_CD");
     }
 
-    public String getConceptCodeIdentifier() throws FileFormatException {
-        return super.getString("MillenniumConceptIdentifier");
+    public String getConceptCodeIdentifier() {
+        return super.getString("CONCEPT_CKI_IDENT");
     }
 
-    public String getConceptCodeType() throws FileFormatException {
-        String conceptCodeIdentifier = super.getString("MillenniumConceptIdentifier");
+    public String getConceptCodeType() {
+        String conceptCodeIdentifier = super.getString("CONCEPT_CKI_IDENT");
         int index = conceptCodeIdentifier.indexOf('!');
         if (index > -1) {
             return conceptCodeIdentifier.substring(0,index);
@@ -101,8 +105,8 @@ public class DIAGN extends AbstractCharacterParser {
         }
     }
 
-    public String getConceptCode() throws FileFormatException {
-        String conceptCodeIdentifier = super.getString("MillenniumConceptIdentifier");
+    public String getConceptCode() {
+        String conceptCodeIdentifier = super.getString("CONCEPT_CKI_IDENT");
         int index = conceptCodeIdentifier.indexOf('!');
         if (index > -1) {
             return conceptCodeIdentifier.substring(index + 1);
@@ -111,16 +115,21 @@ public class DIAGN extends AbstractCharacterParser {
         }
     }
 
-    public String getDiagnosicFreeText() throws FileFormatException {
-        return super.getString("DiagnosisFreeText");
+    public String getDiagnosicFreeText() {
+        return super.getString("DIAGNOSIS_TXT");
     }
 
-    public String getProcedureCodeSequenceEntryOrder() throws FileFormatException {
-        return super.getString("DiagnosisCodeSequenceEntryOrder");
+    public String getProcedureCodeSequenceEntryOrder() {
+        return super.getString("DIAGNOSIS_ENTRY_SEQ_NBR");
     }
 
     @Override
     protected String getFileTypeDescription() {
         return "Cerner diagnosis file";
+    }
+
+    @Override
+    protected boolean isFileAudited() {
+        return true;
     }
 }
