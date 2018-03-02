714bf7fb353a1f8ee31b5a6c1a6833716b2e36c7
Rob Morton <rob@robmorton.org.uk>
2018-03-02 12:19:02 +0000

    Ignore CVREF records that don't have a numeric code.

--- src/main/java/org/endeavourhealth/transform/barts/transforms/CVREFTransformer.java
+++ src/main/java/org/endeavourhealth/transform/barts/transforms/CVREFTransformer.java
@@ -1,5 +1,6 @@
 package org.endeavourhealth.transform.barts.transforms;
 
+import org.apache.commons.lang3.StringUtils;
 import org.endeavourhealth.core.database.dal.DalProvider;
 import org.endeavourhealth.core.database.dal.publisherTransform.CernerCodeValueRefDalI;
 import org.endeavourhealth.core.database.dal.publisherTransform.models.CernerCodeValueRef;
@@ -55,7 +56,10 @@ public class CVREFTransformer {
 
 
     public static void transform(CVREF parser, FhirResourceFiler fhirResourceFiler) throws Exception {
-
+        //For CVREF the first column should always resolve as a numeric code. We've seen some bad data appended to CVREF files
+        if (!StringUtils.isNumeric(parser.getCodeValueCode().getString())) {
+                    return;
+        }
         CsvCell codeValueCode = parser.getCodeValueCode();
         CsvCell date = parser.getDate();
         CsvCell activeInd = parser.getActiveInd();
@@ -83,8 +87,7 @@ public class CVREFTransformer {
         auditWrapper.auditValue(codeDispTxt.getRowAuditId(), codeDispTxt.getColIndex(), DISP_TXT);
         auditWrapper.auditValue(codeDescTxt.getRowAuditId(), codeDescTxt.getColIndex(), DESC_TXT);
         auditWrapper.auditValue(codeMeaningTxt.getRowAuditId(), codeMeaningTxt.getColIndex(), MEANING_TXT);
-        //TODO FYI. CernerCodeValueRef uses sql date. CsvCell uses util date. Deliberate?
-        //Date mapDate = new java.sql.Date(date.getDate().getTime());
+
         byte active = (byte)activeInd.getInt().intValue();
         CernerCodeValueRef mapping = new CernerCodeValueRef(codeValueCode.getLong(),
                                     formattedDate,
