b2861a6af5569bd55ef6054eeedcceec6ed82dbd
Drew Littler <drewlittler@hotmail.com>
2018-04-23 10:52:36 +0100

    Fix to TPP Media file

--- src/main/java/org/endeavourhealth/transform/tpp/csv/schema/patient/SRPatientContactDetails.java
+++ src/main/java/org/endeavourhealth/transform/tpp/csv/schema/patient/SRPatientContactDetails.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 public class SRPatientContactDetails extends AbstractCsvParser {
 
- private static final Logger LOG = LoggerFactory.getLogger(SRPatientContactDetails.class);
+    private static final Logger LOG = LoggerFactory.getLogger(SRPatientContactDetails.class);
 
     public SRPatientContactDetails(UUID serviceId, UUID systemId, UUID exchangeId, String version, String filePath) throws Exception {
         super(serviceId, systemId, exchangeId, version, filePath,
@@ -21,44 +21,109 @@ public class SRPatientContactDetails extends AbstractCsvParser {
 
     @Override
     protected String[] getCsvHeaders(String version) {
-        return new String[]{
-                "RowIdentifier",
-                "IDOrganisationVisibleTo",
-                "DateEventRecorded",
-                "DateEvent",
-                "IDProfileEnteredBy",
-                "IDDoneBy",
-                "TextualEventDoneBy",
-                "IDOrganisationDoneAt",
-                "ContactType",
-                "ContactNumber",
-                "IDEvent",
-                "IDPatient",
-                "IDOrganisation",
-                "RemovedData"
-        };
+        if (version.equals(TppCsvToFhirTransformer.VERSION_87)) {
+            return new String[]{
+                    "RowIdentifier",
+                    "IDOrganisationVisibleTo",
+                    "DateEventRecorded",
+                    "DateEvent",
+                    "IDProfileEnteredBy",
+                    "IDDoneBy",
+                    "TextualEventDoneBy",
+                    "IDOrganisationDoneAt",
+                    "ContactType",
+                    "ContactNumber",
+                    "IDEvent",
+                    "IDPatient",
+                    "IDOrganisation",
+                    "IDOrganisationRegisteredAt",
+                    "RemovedData"
+            };
+        } else {
+            return new String[]{
+                    "RowIdentifier",
+                    "IDOrganisationVisibleTo",
+                    "DateEventRecorded",
+                    "DateEvent",
+                    "IDProfileEnteredBy",
+                    "IDDoneBy",
+                    "TextualEventDoneBy",
+                    "IDOrganisationDoneAt",
+                    "ContactType",
+                    "ContactNumber",
+                    "IDEvent",
+                    "IDPatient",
+                    "IDOrganisation",
+                    "RemovedData"
+            };
+        }
     }
 
-    public CsvCell getRowIdentifier() { return super.getCell("RowIdentifier");}
-    public CsvCell getIDOrganisationVisibleTo() { return super.getCell("IDOrganisationVisibleTo");}
-    public CsvCell getDateEventRecorded() { return super.getCell("DateEventRecorded");}
-    public CsvCell getDateEvent() { return super.getCell("DateEvent");}
-    public CsvCell getIDProfileEnteredBy() { return super.getCell("IDProfileEnteredBy");}
-    public CsvCell getIDDoneBy() { return super.getCell("IDDoneBy");}
-    public CsvCell getTextualEventDoneBy() { return super.getCell("TextualEventDoneBy");}
-    public CsvCell getIDOrganisationDoneAt() { return super.getCell("IDOrganisationDoneAt");}
-    public CsvCell getContactType() { return super.getCell("ContactType");}
-    public CsvCell getContactNumber() { return super.getCell("ContactNumber");}
-    public CsvCell getIDEvent() { return super.getCell("IDEvent");}
-    public CsvCell getIDPatient() { return super.getCell("IDPatient");}
-    public CsvCell getIDOrganisation() { return super.getCell("IDOrganisation");}
-    public CsvCell getRemovedData() { return super.getCell("RemovedData");}
+    public CsvCell getRowIdentifier() {
+        return super.getCell("RowIdentifier");
+    }
+
+    public CsvCell getIDOrganisationVisibleTo() {
+        return super.getCell("IDOrganisationVisibleTo");
+    }
+
+    public CsvCell getDateEventRecorded() {
+        return super.getCell("DateEventRecorded");
+    }
+
+    public CsvCell getDateEvent() {
+        return super.getCell("DateEvent");
+    }
+
+    public CsvCell getIDProfileEnteredBy() {
+        return super.getCell("IDProfileEnteredBy");
+    }
+
+    public CsvCell getIDDoneBy() {
+        return super.getCell("IDDoneBy");
+    }
+
+    public CsvCell getTextualEventDoneBy() {
+        return super.getCell("TextualEventDoneBy");
+    }
+
+    public CsvCell getIDOrganisationDoneAt() {
+        return super.getCell("IDOrganisationDoneAt");
+    }
+
+    public CsvCell getContactType() {
+        return super.getCell("ContactType");
+    }
+
+    public CsvCell getContactNumber() {
+        return super.getCell("ContactNumber");
+    }
+
+    public CsvCell getIDEvent() {
+        return super.getCell("IDEvent");
+    }
+
+    public CsvCell getIDPatient() {
+        return super.getCell("IDPatient");
+    }
+
+    public CsvCell getIDOrganisation() {
+        return super.getCell("IDOrganisation");
+    }
+
+    public CsvCell getRemovedData() {
+        return super.getCell("RemovedData");
+    }
 
 
     //TODO fix the string below to make it meaningful
     @Override
-    protected String getFileTypeDescription() {return "TPP Patient Contact Details Entry file ";}
+    protected String getFileTypeDescription() {
+        return "TPP Patient Contact Details Entry file ";
+    }
 
     @Override
-    protected boolean isFileAudited() {return true;}
+    protected boolean isFileAudited() {
+        return true;
+    }
 }
