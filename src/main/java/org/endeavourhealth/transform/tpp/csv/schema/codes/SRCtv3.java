afd3feb767764eb9f29e219c74a1773833004db4
Drew Littler <drewlittler@hotmail.com>
2018-04-27 10:07:35 +0100

    New file version for TPP

--- src/main/java/org/endeavourhealth/transform/tpp/csv/schema/codes/SRCtv3.java
+++ src/main/java/org/endeavourhealth/transform/tpp/csv/schema/codes/SRCtv3.java
@@ -22,8 +22,7 @@ public class SRCtv3 extends AbstractCsvParser {
     @Override
     protected String[] getCsvHeaders(String version) {
         //TODO - update transform to check for null cells when using fields not in the older version
-        if (version.equals(TppCsvToFhirTransformer.VERSION_TEST_PACK)
-                || version.equals(TppCsvToFhirTransformer.VERSION_87)) {
+        if (version.equals(TppCsvToFhirTransformer.VERSION_TEST_PACK)) {
             return new String[]{
                     "RowIdentifier",
                     "IDOrganisationVisibleTo",
