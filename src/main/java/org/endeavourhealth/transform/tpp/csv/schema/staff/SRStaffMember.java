e742ccb56a125b1016e34f97e2ddeb48758472d6
Rob Morton <rob@robmorton.org.uk>
2018-05-08 19:53:56 +0100

    Add version 89 of schema for version 88 plus RemovedData

--- src/main/java/org/endeavourhealth/transform/tpp/csv/schema/staff/SRStaffMember.java
+++ src/main/java/org/endeavourhealth/transform/tpp/csv/schema/staff/SRStaffMember.java
@@ -21,7 +21,8 @@ public class SRStaffMember extends AbstractCsvParser {
 
     @Override
     protected String[] getCsvHeaders(String version) {
-        if (version.equals(TppCsvToFhirTransformer.VERSION_TEST_PACK)) {
+        if (version.equals(TppCsvToFhirTransformer.VERSION_TEST_PACK)
+                || version.equals(TppCsvToFhirTransformer.VERSION_88)) {
             return new String[]{
                     "RowIdentifier",
                     "StaffName",
@@ -30,6 +31,17 @@ public class SRStaffMember extends AbstractCsvParser {
                     "IDNational",
                     "IDSmartCard",
                     "Obsolete"
+            };
+        }else if (version.equals(TppCsvToFhirTransformer.VERSION_89)) {
+            return new String[]{
+                    "RowIdentifier",
+                    "StaffName",
+                    "StaffUserName",
+                    "NationalIdType",
+                    "IDNational",
+                    "IDSmartCard",
+                    "Obsolete",
+                    "RemovedData"
             };
         } else {
             return new String[]{
