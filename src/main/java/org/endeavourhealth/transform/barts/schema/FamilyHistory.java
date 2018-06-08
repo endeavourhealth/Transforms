32dc70a54673c347cfd018ea0754825bb2d62319
Rob Morton <rob@robmorton.org.uk>
2018-06-08 17:16:36 +0100

    Family history. Schema file and start of transform.

--- src/main/java/org/endeavourhealth/transform/barts/schema/FamilyHistory.java
+++ src/main/java/org/endeavourhealth/transform/barts/schema/FamilyHistory.java
@@ -2,6 +2,7 @@ package org.endeavourhealth.transform.barts.schema;
 
 import org.endeavourhealth.transform.barts.BartsCsvToFhirTransformer;
 import org.endeavourhealth.transform.common.AbstractCsvParser;
+import org.endeavourhealth.transform.common.CsvCell;
 
 import java.util.UUID;
 
@@ -80,6 +81,69 @@ public class FamilyHistory extends AbstractCsvParser {
                 "Record_Updated_Dt"
         };
     }
+
+
+    public CsvCell getFhxActivityKey()  { return super.getCell( "FHX_ACTIVITY_KEY");}
+    public CsvCell getHealthSystemId()  { return super.getCell( "HEALTH_SYSTEM_ID");}
+    public CsvCell getHealthSystemSourceId()  { return super.getCell( "HEALTH_SYSTEM_SOURCE_ID");}
+    public CsvCell getFhxActivityId()  { return super.getCell( "FHX_ACTIVITY_ID");}
+    public CsvCell getFhxActivityGroupId()  { return super.getCell( "FHX_ACTIVITY_GROUP_ID");}
+    public CsvCell getPersonId()  { return super.getCell( "PERSON_ID");}
+    public CsvCell getRelatedPersonId()  { return super.getCell( "RELATED_PERSON_ID");}
+    public CsvCell getRelatedPersonReltnRef()  { return super.getCell( "RELATED_PERSON_RELTN_REF");}
+    public CsvCell getFhxvalueFlag()  { return super.getCell( "FHX_VALUE_FLG");}
+    public CsvCell getOnsetAge()  { return super.getCell( "ONSET_AGE");}
+    public CsvCell getOnsetAgrePrecRef()  { return super.getCell( "ONSET_AGE_PREC_REF");}
+    public CsvCell getOnsetAgeUnitRef()  { return super.getCell( "ONSET_AGE_UNIT_REF");}
+    public CsvCell getActivityOrg()  { return super.getCell( "ACTIVITY_ORG");}
+    public CsvCell getCourseRef()  { return super.getCell( "COURSE_REF");}
+    public CsvCell getLifeCycleStatusRef()  { return super.getCell( "LIFE_CYCLE_STATUS_REF");}
+    public CsvCell getActivityNomen()  { return super.getCell( "ACTIVITY_NOMEN");}
+    public CsvCell getSeverityRef()  { return super.getCell( "SEVERITY_REF");}
+    public CsvCell getTypeMean()  { return super.getCell( "TYPE_MEAN");}
+    public CsvCell getSrcBegEffectDtTm()  { return super.getCell( "SRC_BEG_EFFECT_DT_TM");}
+    public CsvCell getSrcBegEffectTmVldFlg() { return super.getCell( "SRC_BEG_EFFECT_TM_VLD_FLG");}
+    public CsvCell getSrcBegEffectTmZn()  { return super.getCell( "SRC_BEG_EFFECT_TM_ZN");}
+    public CsvCell getSrcEndEffectDtTm()  { return super.getCell( "SRC_END_EFFECT_DT_TM");}
+    public CsvCell getSrcEndEffectTmVldFlg()  { return super.getCell( "SRC_END_EFFECT_TM_VLD_FLG");}
+    public CsvCell getSrcEndEffectTmZn() { return super.getCell( "SRC_END_EFFECT_TM_ZN");}
+    public CsvCell getActiveInd()  { return super.getCell( "ACTIVE_IND");}
+    public CsvCell getTranPrsnlHssId()  { return super.getCell( "TRAN_PRSNL_HSS_ID");}
+    public CsvCell getCreatePrsnl()  { return super.getCell( "CREATE_PRSNL");}
+    public CsvCell getCreateTransPrsnl()  { return super.getCell( "CREATE_TRAN_PRSNL");}
+    public CsvCell getCreateDtTm()  { return super.getCell( "CREATE_DT_TM");}
+    public CsvCell getCreateTmVldFlg()  { return super.getCell( "CREATE_TM_VLD_FLG");}
+    public CsvCell getCreateTmZn()  { return super.getCell( "CREATE_TM_ZN");}
+    public CsvCell getInactivatePrsnl()  { return super.getCell( "INACTIVATE_PRSNL");}
+    public CsvCell getInactivateTranPrsl()  { return super.getCell( "INACTIVATE_TRAN_PRSNL");}
+    public CsvCell getInactivateDtTm()  { return super.getCell( "INACTIVATE_DT_TM");}
+    public CsvCell getInactivateTmVldFlg()  { return super.getCell( "INACTIVATE_TM_VLD_FLG");}
+    public CsvCell getInactivateTmZn()  { return super.getCell( "INACTIVATE_TM_ZN");}
+    public CsvCell getFirstReviewPrsnl()  { return super.getCell( "FIRST_REVIEW_PRSNL");}
+    public CsvCell getFirstReviewTranPrsnl()  { return super.getCell( "FIRST_REVIEW_TRAN_PRSNL");}
+    public CsvCell getFirstReviewDtTM()  { return super.getCell( "FIRST_REVIEW_DT_TM");}
+    public CsvCell getFirstReviewTmVldFlg()  { return super.getCell( "FIRST_REVIEW_TM_VLD_FLG");}
+    public CsvCell getFirstReviewTmZn()  { return super.getCell( "FIRST_REVIEW_TM_ZN");}
+    public CsvCell getLastReviewPrsnl()  { return super.getCell( "LAST_REVIEW_PRSNL");}
+    public CsvCell getLastReviewTranPrsnl()  { return super.getCell( "LAST_REVIEW_TRAN_PRSNL");}
+    public CsvCell getLastReviewDtTM()  { return super.getCell( "LAST_REVIEW_DT_TM");}
+    public CsvCell getLastReviewTmVldFlg()  { return super.getCell( "LAST_REVIEW_TM_VLD_FLG");}
+    public CsvCell getLastReviewTmZn()  { return super.getCell( "LAST_REVIEW_TM_ZN");}
+    public CsvCell getSecurityKey()  { return super.getCell( "SECURITY_KEY");}
+    public CsvCell getSecurityProcessDtTm()  { return super.getCell( "SECURITY_PROCESS_DT_TM");}
+    public CsvCell getDuplicateFlag()  { return super.getCell( "DUPLICATE_FLG");}
+    public CsvCell getOrphanFlag()  { return super.getCell( "ORPHAN_FLG");}
+    public CsvCell getErrorInd()  { return super.getCell( "ERROR_IND");}
+    public CsvCell getFirstProcessDtTm()  { return super.getCell( "FIRST_PROCESS_DT_TM");}
+    public CsvCell getLastProcessDtTm()  { return super.getCell( "LAST_PROCESS_DT_TM");}
+    public CsvCell getTotalUpdates()  { return super.getCell( "TOTAL_UPDATES");}
+    public CsvCell getUpdtDtTm()  { return super.getCell( "UPDT_DT_TM");}
+    public CsvCell getUpdtTask()  { return super.getCell( "UPDT_TASK");}
+    public CsvCell getUpdtUser()  { return super.getCell( "UPDT_USER");}
+    public CsvCell getSourceFlag()  { return super.getCell( "SOURCE_FLG");}
+    public CsvCell getExtractDtTm()  { return super.getCell( "EXTRACT_DT_TM");}
+    public CsvCell getPartitionDtTm()  { return super.getCell( "PARTITION_DT_TM");}
+    public CsvCell getRecordUpdatedDt()  { return super.getCell( "Record_Updated_Dt");}
 
     @Override
     protected String getFileTypeDescription() {
--- src/main/java/org/endeavourhealth/transform/barts/transforms/FamilyHistoryTransformer.java
+++ src/main/java/org/endeavourhealth/transform/barts/transforms/FamilyHistoryTransformer.java
@@ -1,13 +1,24 @@
 package org.endeavourhealth.transform.barts.transforms;
 
 import org.endeavourhealth.transform.barts.BartsCsvHelper;
+import org.endeavourhealth.transform.barts.cache.PatientResourceCache;
 import org.endeavourhealth.transform.barts.schema.FamilyHistory;
+import org.endeavourhealth.transform.common.CsvCell;
 import org.endeavourhealth.transform.common.FhirResourceFiler;
 import org.endeavourhealth.transform.common.ParserI;
+import org.endeavourhealth.transform.common.TransformWarnings;
+import org.endeavourhealth.transform.common.resourceBuilders.FamilyMemberHistoryBuilder;
+import org.endeavourhealth.transform.common.resourceBuilders.PatientBuilder;
+import org.endeavourhealth.transform.ui.helpers.DateHelper;
+import org.endeavourhealth.transform.ui.models.types.UIDate;
+import org.hl7.fhir.instance.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class FamilyHistoryTransformer extends BartsBasisTransformer {
+    private static final Logger LOG = LoggerFactory.getLogger(FamilyHistoryTransformer.class);
 
     public static void transform(String version,
                                  List<ParserI> parsers,
@@ -27,7 +38,42 @@ public class FamilyHistoryTransformer extends BartsBasisTransformer {
         }
     }
 
-    private static void createResource(FamilyHistory parser, FhirResourceFiler fhirResourceFiler, BartsCsvHelper csvHelper, String version, String primaryOrgOdsCode, String primaryOrgHL7OrgOID) {
+    private static void createResource(FamilyHistory parser, FhirResourceFiler fhirResourceFiler, BartsCsvHelper csvHelper,
+                                       String version, String primaryOrgOdsCode, String primaryOrgHL7OrgOID) throws Exception {
+
+        CsvCell milleniumPersonIdCell = parser.getPersonId();
+        if (milleniumPersonIdCell.isEmpty()) {
+            TransformWarnings.log(LOG, parser, "Skipping FamilyHistory record for {} as no patient record found", milleniumPersonIdCell);
+            return;
+        }
+        PatientBuilder patientBuilder = PatientResourceCache.getPatientBuilder(milleniumPersonIdCell, csvHelper);
+
+        if (patientBuilder == null) {
+            TransformWarnings.log(LOG, parser, "Skipping FamilyHistory record for {} as no patient record found", milleniumPersonIdCell);
+            return;
+        }
+
+        FamilyMemberHistoryBuilder familyMemberHistoryBuilder = new FamilyMemberHistoryBuilder();
+        Patient patient = (Patient) patientBuilder.getResource();
+        Reference patientReference = new Reference(patient);
+        familyMemberHistoryBuilder.setPatient(patientReference, milleniumPersonIdCell);
+
+        CsvCell dateTime = parser.getCreateDtTm();
+        if (!dateTime.isEmpty()) {
+            DateTimeType dateTimeType = new DateTimeType(dateTime.getDateTime());
+            familyMemberHistoryBuilder.setDate(dateTimeType, dateTime);
+        }
+
+       //TODO - not convinced it's complete or how we determine status so defaulting to Partial
+        familyMemberHistoryBuilder.setStatus(FamilyMemberHistory.FamilyHistoryStatus.PARTIAL);
+
+        CsvCell relatedPersonCell = parser.getRelatedPersonId();
+        if (!relatedPersonCell.isEmpty()) {
+            // Retrieve related person from cache or db
+            // Need name, relationship, gender
+        }
+
+
 
         //TODO - parser implementation
     }
