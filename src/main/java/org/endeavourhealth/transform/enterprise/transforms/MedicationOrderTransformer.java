e771a23793faf18776450095f862328a1ae2a58c
stuart <stuart@bearclawsolutions.co.uk>
2018-06-14 15:56:44 +0100

    Fix issue with vision data not providing duration and units to medication orders

--- src/main/java/org/endeavourhealth/transform/enterprise/transforms/MedicationOrderTransformer.java
+++ src/main/java/org/endeavourhealth/transform/enterprise/transforms/MedicationOrderTransformer.java
@@ -103,8 +103,10 @@ public class MedicationOrderTransformer extends AbstractTransformer {
 
             if (dispenseRequestComponent.getExpectedSupplyDuration() != null) {
                 Duration duration = dispenseRequestComponent.getExpectedSupplyDuration();
-                if (!duration.getUnit().equalsIgnoreCase("days")) {
-                    throw new TransformException("Unsupported medication order duration type [" + duration.getUnit() + "] for " + fhir.getId());
+                if (duration.getUnit() != null) {
+                    if (!duration.getUnit().equalsIgnoreCase("days")) {
+                        throw new TransformException("Unsupported medication order duration type [" + duration.getUnit() + "] for " + fhir.getId());
+                    }
                 }
                 int days = duration.getValue().intValue();
                 durationDays = Integer.valueOf(days);
