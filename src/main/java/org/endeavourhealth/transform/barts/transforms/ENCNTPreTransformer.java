07781eab790abcc2e9ecc453edbe12eb6a723097
Drew Littler <drewlittler@hotmail.com>
2018-06-29 12:41:26 +0100

    Performance improvement for Cerner ENCNT pre transformer

--- src/main/java/org/endeavourhealth/transform/barts/transforms/ENCNTPreTransformer.java
+++ src/main/java/org/endeavourhealth/transform/barts/transforms/ENCNTPreTransformer.java
@@ -1,13 +1,14 @@
 package org.endeavourhealth.transform.barts.transforms;
 
 import com.google.common.base.Strings;
+import org.endeavourhealth.common.utility.ThreadPool;
+import org.endeavourhealth.common.utility.ThreadPoolError;
 import org.endeavourhealth.core.database.dal.publisherTransform.models.InternalIdMap;
+import org.endeavourhealth.core.database.rdbms.ConnectionManager;
 import org.endeavourhealth.transform.barts.BartsCsvHelper;
 import org.endeavourhealth.transform.barts.BartsCsvToFhirTransformer;
 import org.endeavourhealth.transform.barts.schema.ENCNT;
-import org.endeavourhealth.transform.common.CsvCell;
-import org.endeavourhealth.transform.common.FhirResourceFiler;
-import org.endeavourhealth.transform.common.ParserI;
+import org.endeavourhealth.transform.common.*;
 import org.hl7.fhir.instance.model.ResourceType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,20 +22,30 @@ public class ENCNTPreTransformer {
                                  FhirResourceFiler fhirResourceFiler,
                                  BartsCsvHelper csvHelper) throws Exception {
 
-        for (ParserI parser: parsers) {
-            while (parser.nextRecord()) {
+        //we need to write a lot of stuff to the DB and each record is independent, so use a thread pool to parallelise
+        int threadPoolSize = ConnectionManager.getPublisherCommonConnectionPoolMaxSize();
+        ThreadPool threadPool = new ThreadPool(threadPoolSize, 10000);
 
-                //no try/catch here, because any error means it's not safe to continue
-                processRecord((ENCNT)parser, fhirResourceFiler, csvHelper);
+        try {
+            for (ParserI parser: parsers) {
+                while (parser.nextRecord()) {
+
+                    //no try/catch here, because any error means it's not safe to continue
+                    processRecord((ENCNT)parser, fhirResourceFiler, csvHelper, threadPool);
+                }
             }
+        } finally {
+            List<ThreadPoolError> errors = threadPool.waitAndStop();
+            AbstractCsvCallable.handleErrors(errors);
         }
+
     }
 
     /**
      * this pre-transformer tries to match the Data Warehouse Encounters to the HL7 Receiver Encounters
      * which needs the MRN and VISIT ID
      */
-    public static void processRecord(ENCNT parser, FhirResourceFiler fhirResourceFiler, BartsCsvHelper csvHelper) throws Exception {
+    public static void processRecord(ENCNT parser, FhirResourceFiler fhirResourceFiler, BartsCsvHelper csvHelper, ThreadPool threadPool) throws Exception {
 
         //in-active (i.e. deleted) rows don't have anything else but the ID, so we can't do anything with them
         CsvCell activeCell = parser.getActiveIndicator();
@@ -55,18 +66,51 @@ public class ENCNTPreTransformer {
         CsvCell encounterIdCell = parser.getEncounterId();
         CsvCell personIdCell = parser.getMillenniumPersonIdentifier();
 
-        //the HL7 Receiver uses the MRN as part of the Encounter ID, so we need to look that up
-        String mrn = csvHelper.getInternalId(InternalIdMap.TYPE_MILLENNIUM_PERSON_ID_TO_MRN, personIdCell.getString());
-        if (Strings.isNullOrEmpty(mrn)) {
-            return;
-        }
+        PreTransformCallable callable = new PreTransformCallable(parser.getCurrentState(), personIdCell, encounterIdCell, visitIdCell, csvHelper);
 
-        //the Data Warehouse files all use PersonID as the unique local identifier for patients, but the
-        //ADT feed uses the MRN, so we need to ensure that the Discovery UUID is the same as used by the ADT feed
-        String localUniqueId = encounterIdCell.getString();
-        String hl7ReceiverUniqueId = "PIdAssAuth=" + BartsCsvToFhirTransformer.PRIMARY_ORG_HL7_OID + "-PatIdValue=" + mrn + "-EpIdTypeCode=VISITID-EpIdValue=" + visitIdCell.getString(); //this must match the HL7 Receiver
-        String hl7ReceiverScope = csvHelper.getHl7ReceiverScope();
-        csvHelper.createResourceIdOrCopyFromHl7Receiver(ResourceType.Encounter, localUniqueId, hl7ReceiverUniqueId, hl7ReceiverScope);
+        List<ThreadPoolError> errors = threadPool.submit(callable);
+        AbstractCsvCallable.handleErrors(errors);
     }
 
+
+    static class PreTransformCallable extends AbstractCsvCallable {
+
+        private CsvCell personIdCell;
+        private CsvCell encounterIdCell;
+        private CsvCell visitIdCell;
+        private BartsCsvHelper csvHelper;
+
+        public PreTransformCallable(CsvCurrentState parserState, CsvCell personIdCell, CsvCell encounterIdCell, CsvCell visitIdCell, BartsCsvHelper csvHelper) {
+            super(parserState);
+            this.personIdCell = personIdCell;
+            this.encounterIdCell = encounterIdCell;
+            this.visitIdCell = visitIdCell;
+            this.csvHelper = csvHelper;
+        }
+
+        @Override
+        public Object call() throws Exception {
+
+            try {
+
+                //the HL7 Receiver uses the MRN as part of the Encounter ID, so we need to look that up
+                String mrn = csvHelper.getInternalId(InternalIdMap.TYPE_MILLENNIUM_PERSON_ID_TO_MRN, personIdCell.getString());
+                if (!Strings.isNullOrEmpty(mrn)) {
+
+                    //the Data Warehouse files all use PersonID as the unique local identifier for patients, but the
+                    //ADT feed uses the MRN, so we need to ensure that the Discovery UUID is the same as used by the ADT feed
+                    String localUniqueId = encounterIdCell.getString();
+                    String hl7ReceiverUniqueId = "PIdAssAuth=" + BartsCsvToFhirTransformer.PRIMARY_ORG_HL7_OID + "-PatIdValue=" + mrn + "-EpIdTypeCode=VISITID-EpIdValue=" + visitIdCell.getString(); //this must match the HL7 Receiver
+                    String hl7ReceiverScope = csvHelper.getHl7ReceiverScope();
+                    csvHelper.createResourceIdOrCopyFromHl7Receiver(ResourceType.Encounter, localUniqueId, hl7ReceiverUniqueId, hl7ReceiverScope);
+                }
+
+            } catch (Throwable t) {
+                LOG.error("", t);
+                throw t;
+            }
+
+            return null;
+        }
+    }
 }
