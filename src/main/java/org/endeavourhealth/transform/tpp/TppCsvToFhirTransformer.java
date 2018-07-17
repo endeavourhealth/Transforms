bcadfec1f7bc97ce91e283dc6baebfab16472856
Rob Morton <rob@robmorton.org.uk>
2018-07-17 16:03:12 +0100

    Fix Location id and org id mismatches

--- src/main/java/org/endeavourhealth/transform/tpp/TppCsvToFhirTransformer.java
+++ src/main/java/org/endeavourhealth/transform/tpp/TppCsvToFhirTransformer.java
@@ -323,11 +323,11 @@ public abstract class TppCsvToFhirTransformer {
         LOG.trace("Starting admin transforms");
         // Code lookups
         SRMappingTransformer.transform(parsers, fhirResourceFiler);
-//        SRConfiguredListOptionTransformer.transform(parsers, fhirResourceFiler);
-//        SRMedicationReadCodeDetailsTransformer.transform(parsers, fhirResourceFiler);
-//        SRCtv3HierarchyTransformer.transform(parsers, fhirResourceFiler);
-//        SRCtv3Transformer.transform(parsers, fhirResourceFiler);
-//
+        SRConfiguredListOptionTransformer.transform(parsers, fhirResourceFiler);
+        SRMedicationReadCodeDetailsTransformer.transform(parsers, fhirResourceFiler);
+        SRCtv3HierarchyTransformer.transform(parsers, fhirResourceFiler);
+        SRCtv3Transformer.transform(parsers, fhirResourceFiler);
+
 //        // Organisations
         LocationResourceCache.setServiceId(fhirResourceFiler.getServiceId());
         SRCcgTransformer.transform(parsers, fhirResourceFiler, csvHelper);
