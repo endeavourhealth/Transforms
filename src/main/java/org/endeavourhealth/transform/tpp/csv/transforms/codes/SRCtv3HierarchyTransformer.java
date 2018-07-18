795cf641861acc45bd2cc484bb33c62aa6fa6a4d
Froiland Go <Froiland.Go@learning-health.co.uk>
2018-07-18 17:17:44 +0100

    Implement ThreadPool Ctv3 saving

--- src/main/java/org/endeavourhealth/transform/tpp/csv/transforms/codes/SRCtv3HierarchyTransformer.java
+++ src/main/java/org/endeavourhealth/transform/tpp/csv/transforms/codes/SRCtv3HierarchyTransformer.java
@@ -1,17 +1,20 @@
 package org.endeavourhealth.transform.tpp.csv.transforms.codes;
 
+import org.endeavourhealth.common.utility.ThreadPool;
+import org.endeavourhealth.common.utility.ThreadPoolError;
 import org.endeavourhealth.core.database.dal.DalProvider;
 import org.endeavourhealth.core.database.dal.publisherCommon.TppCtv3HierarchyRefDalI;
 import org.endeavourhealth.core.database.dal.publisherCommon.models.TppCtv3HierarchyRef;
-import org.endeavourhealth.transform.common.AbstractCsvParser;
-import org.endeavourhealth.transform.common.CsvCell;
-import org.endeavourhealth.transform.common.FhirResourceFiler;
-import org.endeavourhealth.transform.common.TransformWarnings;
+import org.endeavourhealth.core.database.rdbms.ConnectionManager;
+import org.endeavourhealth.core.exceptions.TransformException;
+import org.endeavourhealth.transform.common.*;
 import org.endeavourhealth.transform.tpp.csv.schema.codes.SRCtv3Hierarchy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 public class SRCtv3HierarchyTransformer {
     private static final Logger LOG = LoggerFactory.getLogger(SRCtv3HierarchyTransformer.class);
@@ -21,18 +24,41 @@ public class SRCtv3HierarchyTransformer {
     public static void transform(Map<Class, AbstractCsvParser> parsers,
                                  FhirResourceFiler fhirResourceFiler) throws Exception {
 
-        AbstractCsvParser parser = parsers.get(SRCtv3Hierarchy.class);
-        while (parser.nextRecord()) {
+        //we're just streaming content, row by row, into the DB, so use a threadpool to parallelise it
+        int threadPoolSize = ConnectionManager.getPublisherCommonConnectionPoolMaxSize();
+        ThreadPool threadPool = new ThreadPool(threadPoolSize, 10000);
 
-            try {
-                createResource((SRCtv3Hierarchy)parser, fhirResourceFiler);
-            } catch (Exception ex) {
-                fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+
+        try {
+            AbstractCsvParser parser = parsers.get(SRCtv3Hierarchy.class);
+            while (parser.nextRecord()) {
+
+                try {
+                    createResource((SRCtv3Hierarchy)parser, threadPool);
+                } catch (Exception ex) {
+                    fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+                }
             }
+        } finally {
+            List<ThreadPoolError> errors = threadPool.waitAndStop();
+            handleErrors(errors);
         }
     }
 
-    public static void createResource(SRCtv3Hierarchy parser, FhirResourceFiler fhirResourceFiler) throws Exception {
+    private static void handleErrors(List<ThreadPoolError> errors) throws Exception {
+        if (errors == null || errors.isEmpty()) {
+            return;
+        }
+
+        //if we've had multiple errors, just throw the first one, since they'll most-likely be the same
+        ThreadPoolError first = errors.get(0);
+        Throwable exception = first.getException();
+        SRCtv3HierarchyTransformer.WebServiceLookup callable = (SRCtv3HierarchyTransformer.WebServiceLookup)first.getCallable();
+        CsvCurrentState parserState = callable.getParserState();
+        throw new TransformException(parserState.toString(), exception);
+    }
+
+    public static void createResource(SRCtv3Hierarchy parser, ThreadPool threadPool) throws Exception {
 
         CsvCell rowId = parser.getRowIdentifier();
         if (rowId.isEmpty()) {
@@ -59,13 +85,53 @@ public class SRCtv3HierarchyTransformer {
             return;
         }
 
+        List<ThreadPoolError> errors =
+                threadPool.submit(new SRCtv3HierarchyTransformer.WebServiceLookup(
+                        parser.getCurrentState(), rowId, ctv3ParentReadCode, ctv3ChildReadCode, ctv3ChildLevel));
 
-        TppCtv3HierarchyRef ref = new TppCtv3HierarchyRef(rowId.getLong(),
-                ctv3ParentReadCode.getString(),
-                ctv3ChildReadCode.getString(),
-                ctv3ChildLevel.getInt());
+        handleErrors(errors);
+    }
 
-        //save to the DB
-        repository.save(ref);
+    static class WebServiceLookup implements Callable {
+
+        private CsvCurrentState parserState = null;
+        private CsvCell rowId = null;
+        private CsvCell ctv3ParentReadCode = null;
+        private CsvCell ctv3ChildReadCode = null;
+        private CsvCell ctv3ChildLevel = null;
+
+        public WebServiceLookup(CsvCurrentState parserState, CsvCell rowId, CsvCell ctv3ParentReadCode,
+                                CsvCell ctv3ChildReadCode, CsvCell ctv3ChildLevel) {
+
+            this.parserState = parserState;
+            this.rowId = rowId;
+            this.ctv3ParentReadCode = ctv3ParentReadCode;
+            this.ctv3ChildReadCode = ctv3ChildReadCode;
+            this.ctv3ChildLevel = ctv3ChildLevel;
+        }
+
+        @Override
+        public Object call() throws Exception {
+
+            try {
+                TppCtv3HierarchyRef ref = new TppCtv3HierarchyRef(rowId.getLong(),
+                        ctv3ParentReadCode.getString(),
+                        ctv3ChildReadCode.getString(),
+                        ctv3ChildLevel.getInt());
+
+                //save to the DB
+                repository.save(ref);
+
+            } catch (Throwable t) {
+                LOG.error("", t);
+                throw t;
+            }
+
+            return null;
+        }
+
+        public CsvCurrentState getParserState() {
+            return parserState;
+        }
     }
 }
--- src/main/java/org/endeavourhealth/transform/tpp/csv/transforms/codes/SRCtv3Transformer.java
+++ src/main/java/org/endeavourhealth/transform/tpp/csv/transforms/codes/SRCtv3Transformer.java
@@ -1,17 +1,21 @@
 package org.endeavourhealth.transform.tpp.csv.transforms.codes;
 
+import org.endeavourhealth.common.utility.ThreadPool;
+import org.endeavourhealth.common.utility.ThreadPoolError;
 import org.endeavourhealth.core.database.dal.DalProvider;
 import org.endeavourhealth.core.database.dal.publisherCommon.TppCtv3LookupDalI;
 import org.endeavourhealth.core.database.dal.publisherCommon.models.TppCtv3Lookup;
 import org.endeavourhealth.core.database.dal.publisherTransform.models.ResourceFieldMappingAudit;
-import org.endeavourhealth.transform.common.AbstractCsvParser;
-import org.endeavourhealth.transform.common.CsvCell;
-import org.endeavourhealth.transform.common.FhirResourceFiler;
+import org.endeavourhealth.core.database.rdbms.ConnectionManager;
+import org.endeavourhealth.core.exceptions.TransformException;
+import org.endeavourhealth.transform.common.*;
 import org.endeavourhealth.transform.tpp.csv.schema.codes.SRCtv3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.Callable;
 
 public class SRCtv3Transformer {
     private static final Logger LOG = LoggerFactory.getLogger(SRCtv3Transformer.class);
@@ -24,36 +28,98 @@ public class SRCtv3Transformer {
     public static void transform(Map<Class, AbstractCsvParser> parsers,
                                  FhirResourceFiler fhirResourceFiler) throws Exception {
 
-        AbstractCsvParser parser = parsers.get(SRCtv3.class);
-        while (parser.nextRecord()) {
 
-            try {
-                createResource((SRCtv3) parser, fhirResourceFiler);
-            } catch (Exception ex) {
-                fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+        //we're just streaming content, row by row, into the DB, so use a threadpool to parallelise it
+        int threadPoolSize = ConnectionManager.getPublisherCommonConnectionPoolMaxSize();
+        ThreadPool threadPool = new ThreadPool(threadPoolSize, 10000);
+
+        try {
+            AbstractCsvParser parser = parsers.get(SRCtv3.class);
+            while (parser.nextRecord()) {
+
+                try {
+                    createResource((SRCtv3) parser, threadPool);
+                } catch (Exception ex) {
+                    fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+                }
             }
+        } finally {
+            List<ThreadPoolError> errors = threadPool.waitAndStop();
+            handleErrors(errors);
         }
     }
 
-    public static void createResource(SRCtv3 parser, FhirResourceFiler fhirResourceFiler) throws Exception {
+    private static void handleErrors(List<ThreadPoolError> errors) throws Exception {
+        if (errors == null || errors.isEmpty()) {
+            return;
+        }
+
+        //if we've had multiple errors, just throw the first one, since they'll most-likely be the same
+        ThreadPoolError first = errors.get(0);
+        Throwable exception = first.getException();
+        SRCtv3Transformer.WebServiceLookup callable = (SRCtv3Transformer.WebServiceLookup)first.getCallable();
+        CsvCurrentState parserState = callable.getParserState();
+        throw new TransformException(parserState.toString(), exception);
+    }
+
+    public static void createResource(SRCtv3 parser, ThreadPool threadPool) throws Exception {
 
         CsvCell rowId = parser.getRowIdentifier();
+        if (rowId.isEmpty()) {
+            TransformWarnings.log(LOG, parser, "ERROR: invalid row Identifier: {} in file : {}",
+                    rowId.getString(), parser.getFilePath());
+            return;
+        }
         CsvCell ctv3Code = parser.getCtv3Code();
         CsvCell ctv3Text = parser.getCtv3Text();
 
-        ResourceFieldMappingAudit auditWrapper = new ResourceFieldMappingAudit();
+        List<ThreadPoolError> errors =
+                threadPool.submit(new SRCtv3Transformer.WebServiceLookup(
+                        parser.getCurrentState(), rowId, ctv3Code, ctv3Text));
 
-        auditWrapper.auditValue(rowId.getRowAuditId(), rowId.getColIndex(), ROW_ID);
-        auditWrapper.auditValue(ctv3Code.getRowAuditId(), ctv3Code.getColIndex(), CTV3_CODE);
-        auditWrapper.auditValue(ctv3Text.getRowAuditId(), ctv3Text.getColIndex(), CTV3_TEXT);
+        handleErrors(errors);
+    }
 
-        TppCtv3Lookup lookup = new TppCtv3Lookup(rowId.getLong(),
-                ctv3Code.getString(),
-                ctv3Text.getString(),
-                auditWrapper);
+    static class WebServiceLookup implements Callable {
 
-        //save to the DB
-        repository.save(lookup);
+        private CsvCurrentState parserState = null;
+        private CsvCell rowId = null;
+        private CsvCell ctv3Code = null;
+        private CsvCell ctv3Text = null;
 
+        public WebServiceLookup(CsvCurrentState parserState, CsvCell rowId, CsvCell ctv3Code, CsvCell ctv3Text) {
+            this.parserState = parserState;
+            this.rowId = rowId;
+            this.ctv3Code = ctv3Code;
+            this.ctv3Text = ctv3Text;
+        }
+
+        @Override
+        public Object call() throws Exception {
+
+            try {
+                ResourceFieldMappingAudit auditWrapper = new ResourceFieldMappingAudit();
+
+                auditWrapper.auditValue(rowId.getRowAuditId(), rowId.getColIndex(), ROW_ID);
+                auditWrapper.auditValue(ctv3Code.getRowAuditId(), ctv3Code.getColIndex(), CTV3_CODE);
+                auditWrapper.auditValue(ctv3Text.getRowAuditId(), ctv3Text.getColIndex(), CTV3_TEXT);
+
+                TppCtv3Lookup lookup =
+                        new TppCtv3Lookup(rowId.getLong(), ctv3Code.getString(), ctv3Text.getString(), auditWrapper);
+
+                //save to the DB
+                repository.save(lookup);
+
+            } catch (Throwable t) {
+                LOG.error("", t);
+                throw t;
+            }
+
+            return null;
+        }
+
+        public CsvCurrentState getParserState() {
+            return parserState;
+        }
     }
 }
