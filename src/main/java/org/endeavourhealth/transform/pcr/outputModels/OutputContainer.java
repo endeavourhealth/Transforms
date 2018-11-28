c2ab8309a5f4183d7ab744c6b4800d6d3935a2c5
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2018-11-28 12:20:25 +0000

    Fix to filenames

--- src/main/java/org/endeavourhealth/transform/pcr/outputModels/OutputContainer.java
+++ src/main/java/org/endeavourhealth/transform/pcr/outputModels/OutputContainer.java
@@ -46,10 +46,10 @@ public class OutputContainer {
         //csvWriters.add(new Schedule("schedule.csv", csvFormat, dateFormat, timeFormat));
         //csvWriters.add(new Person("person.csv", csvFormat, dateFormat, timeFormat, pseduonymised));
         csvWriters.add(new Patient("patient.csv", csvFormat, dateFormat, timeFormat));
-        csvWriters.add(new PatientAddress("patientAddress.csv", csvFormat, dateFormat, timeFormat));
+        csvWriters.add(new PatientAddress("patient_address.csv", csvFormat, dateFormat, timeFormat));
         csvWriters.add(new Address("address.csv", csvFormat, dateFormat, timeFormat));
-        csvWriters.add(new PatientContact("patientContact.csv", csvFormat, dateFormat, timeFormat));
-        csvWriters.add(new PatientIdentifier("patientIdentifier.csv", csvFormat, dateFormat, timeFormat));
+        csvWriters.add(new PatientContact("patient_contact.csv", csvFormat, dateFormat, timeFormat));
+        csvWriters.add(new PatientIdentifier("patient_identifier.csv", csvFormat, dateFormat, timeFormat));
 
         //TODO - need EventRelationship
 
