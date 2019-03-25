58b5a7797eb3df0afddddae70c65b4e3419aa31b
Drew Littler <drewlittler@hotmail.com>
2019-03-25 19:26:23 +0000

    Fix to Emis->FHIR transform for cancelling of appointments

--- src/main/java/org/endeavourhealth/transform/emis/csv/transforms/appointment/SlotTransformer.java
+++ src/main/java/org/endeavourhealth/transform/emis/csv/transforms/appointment/SlotTransformer.java
@@ -1,7 +1,10 @@
 package org.endeavourhealth.transform.emis.csv.transforms.appointment;
 
+import com.google.common.base.Strings;
 import org.endeavourhealth.common.fhir.QuantityHelper;
 import org.endeavourhealth.common.fhir.ReferenceHelper;
+import org.endeavourhealth.core.database.dal.DalProvider;
+import org.endeavourhealth.core.database.dal.publisherTransform.InternalIdDalI;
 import org.endeavourhealth.transform.common.AbstractCsvParser;
 import org.endeavourhealth.transform.common.CsvCell;
 import org.endeavourhealth.transform.common.FhirResourceFiler;
@@ -20,6 +23,10 @@ import org.hl7.fhir.instance.model.ResourceType;
 import java.util.*;
 
 public class SlotTransformer {
+
+    private static final String SLOT_LATEST_PATIENT_GUID = "EmisSlotLatestPatientGuid";
+
+    private static final InternalIdDalI internalIdDal = DalProvider.factoryInternalIdDal();
 
     public static void transform(String version,
                                  Map<Class, AbstractCsvParser> parsers,
@@ -44,7 +51,11 @@ public class SlotTransformer {
                                                  FhirResourceFiler fhirResourceFiler,
                                                  EmisCsvHelper csvHelper) throws Exception {
 
+        CsvCell slotGuid = parser.getSlotGuid();
         CsvCell patientGuid = parser.getPatientGuid();
+
+        //see if our slot previously had a different patient
+        cancelPreviousAppointmentIfNecessary(slotGuid, patientGuid, csvHelper, fhirResourceFiler);
 
         //the slots CSV contains data on empty slots too; ignore them
         if (patientGuid.isEmpty()) {
@@ -58,7 +69,6 @@ public class SlotTransformer {
             return;
         }
 
-        CsvCell slotGuid = parser.getSlotGuid();
 
         SlotBuilder slotBuilder = new SlotBuilder();
         AppointmentBuilder appointmentBuilder = new AppointmentBuilder();
@@ -68,7 +78,7 @@ public class SlotTransformer {
         EmisCsvHelper.setUniqueId(appointmentBuilder, patientGuid, slotGuid);
 
         //moved this higher up, because we need to have set the patient ID on the resource before we delete it
-        Reference patientReference = csvHelper.createPatientReference(patientGuid);
+        Reference patientReference = EmisCsvHelper.createPatientReference(patientGuid);
         appointmentBuilder.addParticipant(patientReference, Appointment.ParticipationStatus.ACCEPTED, patientGuid);
 
         //if the Resource is to be deleted from the data store, then stop processing the CSV row
@@ -171,6 +181,54 @@ public class SlotTransformer {
 
         fhirResourceFiler.savePatientResource(parser.getCurrentState(), slotBuilder, appointmentBuilder);
     }
+
+    private static void cancelPreviousAppointmentIfNecessary(CsvCell slotGuid, CsvCell patientGuid, EmisCsvHelper csvHelper, FhirResourceFiler fhirResourceFiler) throws Exception {
+
+        String previousPatientGuid = findPreviousPatientGuidForSlot(slotGuid, csvHelper);
+        if (!Strings.isNullOrEmpty(previousPatientGuid)
+                && !previousPatientGuid.equals(patientGuid.getString())) {
+
+            cancelPreviousAppointment(slotGuid, previousPatientGuid, csvHelper, fhirResourceFiler);
+        }
+
+        //save the current patient GUID for the slot, so we can detect cancellations and rebooking later
+        storeCurrentPatientGuidForSlot(slotGuid, patientGuid, csvHelper);
+
+    }
+
+    /**
+     * if we detect that the patientGUID has changed, this means the appointment was cancelled,
+     * and we need to find the previous Appointment resource and mark it as such.
+     */
+    private static void cancelPreviousAppointment(CsvCell slotGuid, String previousPatientGuid, EmisCsvHelper csvHelper, FhirResourceFiler fhirResourceFiler) throws Exception {
+
+        CsvCell previousPatientGuidCell = CsvCell.factoryDummyWrapper(previousPatientGuid);
+        String sourceId = EmisCsvHelper.createUniqueId(previousPatientGuidCell, slotGuid);
+        Appointment appointment = (Appointment)csvHelper.retrieveResource(sourceId, ResourceType.Appointment);
+
+        //if the appointment has already been deleted or is cancelled or DNA, then do nothing
+        if (appointment == null
+                || appointment.getStatus() == Appointment.AppointmentStatus.CANCELLED
+                || appointment.getStatus() == Appointment.AppointmentStatus.NOSHOW) {
+            return;
+        }
+
+        AppointmentBuilder appointmentBuilder = new AppointmentBuilder(appointment);
+        appointmentBuilder.setStatus(Appointment.AppointmentStatus.CANCELLED);
+
+        //save without mapping IDs as this has been retrieved from the DB
+        fhirResourceFiler.savePatientResource(null, false, appointmentBuilder);
+    }
+
+
+    private static String findPreviousPatientGuidForSlot(CsvCell slotGuidCell, EmisCsvHelper csvHelper) throws Exception {
+        return internalIdDal.getDestinationId(csvHelper.getServiceId(), SLOT_LATEST_PATIENT_GUID, slotGuidCell.getString());
+    }
+
+    private static void storeCurrentPatientGuidForSlot(CsvCell slotGuidCell, CsvCell patientGuidCell, EmisCsvHelper csvHelper) throws Exception {
+        internalIdDal.save(csvHelper.getServiceId(), SLOT_LATEST_PATIENT_GUID, slotGuidCell.getString(), patientGuidCell.getString());
+    }
+
 
     private static List<CsvCell> retrieveExistingPractitioners(CsvCell slotGuidCell, EmisCsvHelper csvHelper, FhirResourceFiler fhirResourceFiler) throws Exception {
 
