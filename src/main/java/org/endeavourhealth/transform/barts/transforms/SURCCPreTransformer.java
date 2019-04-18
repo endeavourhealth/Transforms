d15074e1040603a2b79d270422d9b16e77afa20d
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2019-04-18 12:32:08 +0100

    SURCC (UKRWH_CDE_SURGICAL_CASE) and SURCP (UKRWH_CDE_SURGICAL_CASE_PROCEDURE) PreTransformers

---
+++ src/main/java/org/endeavourhealth/transform/barts/transforms/SURCCPreTransformer.java
@@ -0,0 +1,91 @@
+package org.endeavourhealth.transform.barts.transforms;
+
+import org.endeavourhealth.core.database.dal.DalProvider;
+import org.endeavourhealth.core.database.dal.publisherStaging.StagingSURCCDalI;
+import org.endeavourhealth.core.database.dal.publisherStaging.models.StagingSURCC;
+import org.endeavourhealth.transform.barts.BartsCsvHelper;
+import org.endeavourhealth.transform.barts.schema.SURCC;
+import org.endeavourhealth.transform.common.AbstractCsvCallable;
+import org.endeavourhealth.transform.common.CsvCurrentState;
+import org.endeavourhealth.transform.common.FhirResourceFiler;
+import org.endeavourhealth.transform.common.ParserI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class SURCCPreTransformer {
+    private static final Logger LOG = LoggerFactory.getLogger(SURCCPreTransformer.class);
+
+    private static StagingSURCCDalI repository = DalProvider.factoryStagingSURCCDalI();
+
+    public static void transform(List<ParserI> parsers,
+                                 FhirResourceFiler fhirResourceFiler,
+                                 BartsCsvHelper csvHelper) throws Exception {
+        for (ParserI parser : parsers) {
+
+            while (parser.nextRecord()) {
+                try {
+                    processRecord((SURCC) parser, csvHelper);
+
+                } catch (Exception ex) {
+                    fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+                }
+            }
+        }
+    }
+
+    private static void processRecord(SURCC parser, BartsCsvHelper csvHelper) throws Exception {
+
+        StagingSURCC stagingSURCC = new StagingSURCC();
+        stagingSURCC.setExchangeId(parser.getExchangeId().toString());
+        stagingSURCC.setDTReceived(new Date());
+
+        stagingSURCC.setSurgicalCaseId(parser.getSurgicalCaseId().getInt());
+        stagingSURCC.setDTExtract(parser.getExtractDateTime().getDate());
+        stagingSURCC.setActiveInd(parser.getActiveIndicator().getBoolean());
+        stagingSURCC.setPersonId(parser.getPersonId().getInt());
+        stagingSURCC.setEncounterId(parser.getEncounterId().getInt());
+        stagingSURCC.setDTCancelled(parser.getCancelledDateTime().getDate());
+        stagingSURCC.setInstitutionCode(parser.getInstitutionCode().getString());
+        stagingSURCC.setDepartmentCode(parser.getDepartmentCode().getString());
+        stagingSURCC.setSurgicalAreaCode(parser.getSurgicalAreaCode().getString());
+        stagingSURCC.setTheatreNumberCode(parser.getTheatreNumberCode().getString());
+
+        stagingSURCC.setRecordChecksum(stagingSURCC.hashCode());
+
+        UUID serviceId = csvHelper.getServiceId();
+
+        csvHelper.submitToThreadPool(new SURCCPreTransformer.saveDataCallable(parser.getCurrentState(), stagingSURCC, serviceId));
+    }
+
+    private static class saveDataCallable extends AbstractCsvCallable {
+
+        private StagingSURCC obj = null;
+        private UUID serviceId;
+
+        public saveDataCallable(CsvCurrentState parserState,
+                                StagingSURCC obj,
+                                UUID serviceId) {
+            super(parserState);
+            this.obj = obj;
+            this.serviceId = serviceId;
+        }
+
+        @Override
+        public Object call() throws Exception {
+
+            try {
+                repository.save(obj, serviceId);
+
+            } catch (Throwable t) {
+                LOG.error("", t);
+                throw t;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/main/java/org/endeavourhealth/transform/barts/transforms/SURCPPreTransformer.java
@@ -0,0 +1,93 @@
+package org.endeavourhealth.transform.barts.transforms;
+
+import org.endeavourhealth.core.database.dal.DalProvider;
+import org.endeavourhealth.core.database.dal.publisherStaging.StagingSURCPDalI;
+import org.endeavourhealth.core.database.dal.publisherStaging.models.StagingSURCP;
+import org.endeavourhealth.transform.barts.BartsCsvHelper;
+import org.endeavourhealth.transform.barts.schema.SURCP;
+import org.endeavourhealth.transform.common.AbstractCsvCallable;
+import org.endeavourhealth.transform.common.CsvCurrentState;
+import org.endeavourhealth.transform.common.FhirResourceFiler;
+import org.endeavourhealth.transform.common.ParserI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class SURCPPreTransformer {
+    private static final Logger LOG = LoggerFactory.getLogger(SURCPPreTransformer.class);
+
+    private static StagingSURCPDalI repository = DalProvider.factoryStagingSURCPDalI();
+
+    public static void transform(List<ParserI> parsers,
+                                 FhirResourceFiler fhirResourceFiler,
+                                 BartsCsvHelper csvHelper) throws Exception {
+        for (ParserI parser : parsers) {
+
+            while (parser.nextRecord()) {
+                try {
+                    processRecord((SURCP) parser, csvHelper);
+
+                } catch (Exception ex) {
+                    fhirResourceFiler.logTransformRecordError(ex, parser.getCurrentState());
+                }
+            }
+        }
+    }
+
+    private static void processRecord(SURCP parser, BartsCsvHelper csvHelper) throws Exception {
+
+        StagingSURCP stagingSURCP = new StagingSURCP();
+        stagingSURCP.setExchangeId(parser.getExchangeId().toString());
+        stagingSURCP.setDTReceived(new Date());
+
+        stagingSURCP.setSurgicalCaseProcedureId(parser.getSurgicalCaseProcedureId().getInt());
+        stagingSURCP.setSurgicalCaseId(parser.getSurgicalCaseId().getInt());
+        stagingSURCP.setDTExtract(parser.getExtractDateTime().getDate());
+        stagingSURCP.setActiveInd(parser.getActiveIndicator().getBoolean());
+        stagingSURCP.setProcedureCode(parser.getProcedureCode().getInt());
+        stagingSURCP.setProcedureText(parser.getProcedureText().getString());
+        stagingSURCP.setModifierText(parser.getModifierText().getString());
+        stagingSURCP.setPrimaryProcedureIndicator(parser.getPrimaryProcedureIndicator().getInt());
+        stagingSURCP.setSurgeonPersonnelId(parser.getSurgeonPersonnelId().getInt());
+        stagingSURCP.setDTStart(parser.getStartDateTime().getDate());
+        stagingSURCP.setDTStop(parser.getStopDateTime().getDate());
+        stagingSURCP.setWoundClassCode(parser.getWoundClassCode().getString());
+
+        stagingSURCP.setRecordChecksum(stagingSURCP.hashCode());
+
+        UUID serviceId = csvHelper.getServiceId();
+
+        csvHelper.submitToThreadPool(new SURCPPreTransformer.saveDataCallable(parser.getCurrentState(), stagingSURCP, serviceId));
+    }
+
+    private static class saveDataCallable extends AbstractCsvCallable {
+
+        private StagingSURCP obj = null;
+        private UUID serviceId;
+
+        public saveDataCallable(CsvCurrentState parserState,
+                                StagingSURCP obj,
+                                UUID serviceId) {
+            super(parserState);
+            this.obj = obj;
+            this.serviceId = serviceId;
+        }
+
+        @Override
+        public Object call() throws Exception {
+
+            try {
+                repository.save(obj, serviceId);
+
+            } catch (Throwable t) {
+                LOG.error("", t);
+                throw t;
+            }
+
+            return null;
+        }
+    }
+}
