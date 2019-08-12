47544bf02e8ff1d231621e499a6a487858fbc31a
Drew Littler <drewlittler@hotmail.com>
2019-08-12 16:54:12 +0100

    Typo fix in Adastra transform

--- src/main/java/org/endeavourhealth/transform/adastra/csv/schema/ElectronicPrescriptions.java
+++ src/main/java/org/endeavourhealth/transform/adastra/csv/schema/ElectronicPrescriptions.java
@@ -32,7 +32,7 @@ public class ElectronicPrescriptions extends AbstractCsvParser {
                     "CancellationReasonCode",
                     "CancellationReasonText",
                     "PharmacyName",
-                    "PrescriberNumber ",
+                    "PrescriberNumber",
                     "PrescriberName"
             };
     }
