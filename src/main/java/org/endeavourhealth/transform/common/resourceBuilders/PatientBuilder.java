d4b26da804f209f3c55fa92ff8705767aacf932b
robrab <rob@robmorton.org.uk>
2019-11-15 14:28:59 +0000

    Revert kludge

--- src/main/java/org/endeavourhealth/transform/common/resourceBuilders/PatientBuilder.java
+++ src/main/java/org/endeavourhealth/transform/common/resourceBuilders/PatientBuilder.java
@@ -294,11 +294,7 @@ public class PatientBuilder extends ResourceBuilderBase
         if (coding == null) {
             return null;
         }
-        //TODO remove this kludge to handle bad code set in hl7 postgre tables once current exchange finished
-        if (coding.getCode().equals("black-inc-black-british-african")) {
-            coding.setCode("P");
-        }
-        return EthnicCategory.fromCode(coding.getCode());
+            return EthnicCategory.fromCode(coding.getCode());
     }
 
     public void setEthnicity(EthnicCategory fhirEthnicity, CsvCell... sourceCells) {
