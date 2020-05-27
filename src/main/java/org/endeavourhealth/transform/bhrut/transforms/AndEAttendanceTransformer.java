67c32df02901f02f20dc5f8c19ea4ed9cf6932d9
Leigh Ellwood <leigh.ellwood@ntlworld.com>
2020-05-27 15:00:24 +0100

    Bhrut - A&E encounters updated and further TODO added

--- src/main/java/org/endeavourhealth/transform/bhrut/transforms/AndEAttendanceTransformer.java
+++ src/main/java/org/endeavourhealth/transform/bhrut/transforms/AndEAttendanceTransformer.java
@@ -1,5 +1,7 @@
 package org.endeavourhealth.transform.bhrut.transforms;
 
+import org.endeavourhealth.common.fhir.FhirCodeUri;
+import org.endeavourhealth.core.exceptions.TransformException;
 import org.endeavourhealth.transform.bhrut.BhrutCsvHelper;
 import org.endeavourhealth.transform.bhrut.schema.AandeAttendances;
 import org.endeavourhealth.transform.bhrut.schema.Spells;
@@ -61,11 +63,9 @@ public class AndEAttendanceTransformer {
         Reference patientReference = csvHelper.createPatientReference(patientIdCell);
         encounterBuilder.setPatient(patientReference, patientIdCell);
         CsvCell actionCell = parser.getLinestatus();
-        if (actionCell.getString().equalsIgnoreCase("Delete")) {
-            encounterBuilder.setDeletedAudit(actionCell);
-            fhirResourceFiler.deletePatientResource(parser.getCurrentState(), encounterBuilder);
-            return;
-        }
+        encounterBuilder.setDeletedAudit(actionCell);
+
+        fhirResourceFiler.deletePatientResource(parser.getCurrentState(), encounterBuilder);
     }
 
     public static void createResources(AandeAttendances parser,
@@ -88,29 +88,123 @@ public class AndEAttendanceTransformer {
         Reference orgReference = csvHelper.createOrganisationReference(org.getString());
         encounterBuilder.setServiceProvider(orgReference);
 
-        if (!parser.getReferralSource().isEmpty()) {
-            //TODO Need to know possible values. Sample has
-            //Self Referred
-            //Other
+        //TODO - the ATTENDANCE_NUMBER,  maybe an identifier?
+
+        //set the encounter extensions
+        CsvCell arrivalModeCell = parser.getArrivalMode();
+        if (!arrivalModeCell.isEmpty()) {
+
+            CodeableConceptBuilder cc
+                    = new CodeableConceptBuilder(encounterBuilder, CodeableConceptBuilder.Tag.Encounter_AE_Arrival_Mode);
+            cc.addCoding(FhirCodeUri.CODE_SYSTEM_NHS_DD);
+
+            //there are only two NHS codes for Arrival mode:
+            //1 - Brought in by Emergency Ambulance (including helicopter/'Air Ambulance') , 2 - Other
+            String arrivalModeText = arrivalModeCell.getString();
+            if (arrivalModeText.toLowerCase().contains("ambulance")) {
+                cc.setCodingCode("1");
+                cc.setCodingDisplay("Brought in by Emergency Ambulance (including helicopter/'Air Ambulance')");
+            } else {
+                cc.setCodingCode("2");
+                cc.setCodingDisplay("Other");
+            }
+
+            //original text from the extract file
+            cc.setText(arrivalModeText, arrivalModeCell);
         }
 
-        if (!parser.getArrivalMode().isEmpty()) {
-            //TODO Need to know set of values.  S3 data has
-            //Ambulance Other LAS
-            //Private Transport
-            //Other
+        CsvCell attendanceTypeCell = parser.getAttendanceType();
+        if (!attendanceTypeCell.isEmpty()) {
+
+            CodeableConceptBuilder cc
+                    = new CodeableConceptBuilder(encounterBuilder, CodeableConceptBuilder.Tag.Encounter_AE_Attendance_Category);
+            cc.addCoding(FhirCodeUri.CODE_SYSTEM_NHS_DD);
+            if (attendanceTypeCell.getString().equalsIgnoreCase("1")) {
+
+                cc.setCodingCode("1", attendanceTypeCell);
+                cc.setCodingCode("First Accident and Emergency Attendance - the first in a series, or the only attendance, in a particular Accident and Emergency Episode");
+            } else if (attendanceTypeCell.getString().equalsIgnoreCase("2")) {
+
+                cc.setCodingCode("2", attendanceTypeCell);
+                cc.setCodingCode("Follow-up Accident and Emergency Attendance - planned: a subsequent planned attendance at the same department, and for the same incident as the first attendance");
+            } else if (attendanceTypeCell.getString().equalsIgnoreCase("3")) {
+
+                cc.setCodingCode("3", attendanceTypeCell);
+                cc.setCodingCode("Follow-up Accident and Emergency Attendance - unplanned: a subsequent unplanned attendance at the same department, and for the same incident as the first attendance");
+            } else {
+
+                throw new TransformException("Unexpected attendance type/category code: [" + attendanceTypeCell.getString() + "]");
+            }
+            //there is no original text for this code so do not setText
         }
-        //TODO Discharge method? Sample spells file nothing whose values match those in spells file.
-        if (!parser.getAttendanceType().isEmpty()) {
-            CodeableConceptBuilder cc = new CodeableConceptBuilder(encounterBuilder, CodeableConceptBuilder.Tag.Encounter_AE_Attendance_Category);
-            cc.setText(parser.getAttendanceType().getString(), parser.getAttendanceType());
+
+        CsvCell referralSourceCell = parser.getReferralSource();
+        if (!referralSourceCell.isEmpty()) {
+            CodeableConceptBuilder cc
+                    = new CodeableConceptBuilder(encounterBuilder, CodeableConceptBuilder.Tag.Encounter_AE_Attendance_Source);
+
+            cc.addCoding(FhirCodeUri.CODE_SYSTEM_NHS_DD);
+            String nhsReferralSourceCode = convertReferralSourceText(referralSourceCell.getString());
+            cc.setCodingCode(nhsReferralSourceCode);
+            cc.setCodingDisplay(lookupReferralSource(nhsReferralSourceCode));
+            cc.setText(referralSourceCell.getString(), parser.getReferralSource());
         }
-        if (!parser.getReferralSource().isEmpty()) {
-            CodeableConceptBuilder cc = new CodeableConceptBuilder(encounterBuilder, CodeableConceptBuilder.Tag.Encounter_AE_Attendance_Source);
-            cc.setText(parser.getReferralSource().getString(), parser.getReferralSource());
-        }
+
+        //TODO - analysis data to see if there are sub-encounters possible to model, i.e. treatments
+        //TODO - the chief complaint needs capturing
+
+        //TODO - Use RECORDED_OUTCOME for a&e to determine this element?
+        //encounterBuilder.setDischargeDisposition(?);
+
         fhirResourceFiler.savePatientResource(parser.getCurrentState(), encounterBuilder);
     }
 
+    private static String convertReferralSourceText(String referralSourceText) {
 
+        if (referralSourceText.toLowerCase().contains("gp") ||
+                referralSourceText.toLowerCase().contains("general medical practitioner") )
+            return "00";
+        else if (referralSourceText.toLowerCase().contains("self referral") ||
+                referralSourceText.toLowerCase().contains("self referred"))
+            return "01";
+        else if (referralSourceText.toLowerCase().contains("social services"))
+            return "02";
+        else if (referralSourceText.toLowerCase().contains("emergency services"))
+            return "03";
+        else if (referralSourceText.toLowerCase().contains("work"))
+            return "04";
+        else if (referralSourceText.toLowerCase().contains("educational"))
+            return "05";
+        else if (referralSourceText.toLowerCase().contains("police"))
+            return "06";
+        else if (referralSourceText.toLowerCase().contains("clinic") ||
+                referralSourceText.toLowerCase().contains("hospital") ||
+                referralSourceText.toLowerCase().contains("nursing home"))
+            return "07";
+        else if (referralSourceText.toLowerCase().contains("dentist") ||
+                referralSourceText.toLowerCase().contains("general dental practitioner"))
+            return "92";
+        else if (referralSourceText.toLowerCase().contains("community dental"))
+            return "93";
+
+        return "08";   //Other
+    }
+
+    private static String lookupReferralSource(String referralSourceCode) {
+
+        switch (referralSourceCode) {
+            case "00" : return "GENERAL MEDICAL PRACTITIONER";
+            case "01" : return "Self referral";
+            case "02" : return "Local Authority Social Services";
+            case "03" : return "Emergency services";
+            case "04" : return "Work";
+            case "05" : return "Educational Establishment";
+            case "06" : return "Police";
+            case "07" : return "Health Care Provider: same or other";
+            case "08" : return "Other";
+            case "92" : return "GENERAL DENTAL PRACTITIONER ";
+            case "93" : return "Community Dental Service";
+            default : return null;
+        }
+    }
 }
