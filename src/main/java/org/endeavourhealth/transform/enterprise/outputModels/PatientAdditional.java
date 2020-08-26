89031b3b4754a8f2cc344923d593ce6153ad3fac
robrab <rob@robmorton.org.uk>
2020-08-26 15:49:56 +0100

    Mainly BHRUT specific fixes but also IdMapperEncounter: map partOF rferences ResourceValidatorSlot: check Slot has Schedule. Added support for new patient_additional table, basically a copy of encounter_additional.

---
+++ src/main/java/org/endeavourhealth/transform/enterprise/outputModels/PatientAdditional.java
@@ -0,0 +1,47 @@
+package org.endeavourhealth.transform.enterprise.outputModels;
+
+import org.apache.commons.csv.CSVFormat;
+
+public class PatientAdditional extends AbstractEnterpriseCsvWriter {
+
+    public PatientAdditional(String fileName, CSVFormat csvFormat, String dateFormat, String timeFormat) throws Exception {
+        super(fileName, csvFormat, dateFormat, timeFormat);
+    }
+
+    public void writeDelete(long id) throws Exception {
+
+        super.printRecord(OutputContainer.DELETE,
+                "" + id);
+    }
+
+    public void writeUpsert(long id,
+                            String propertyId,
+                            String valueId
+                            ) throws Exception {
+
+        super.printRecord(OutputContainer.UPSERT,
+                "" + id,
+                "" + propertyId,
+                "" + valueId);   }
+
+
+    @Override
+    public String[] getCsvHeaders() {
+        return new String[] {
+                "save_mode",
+                "id",
+                "property_id",
+                "value_id",
+        };
+    }
+
+    @Override
+    public Class[] getColumnTypes() {
+        return new Class[] {
+                String.class,
+                Long.TYPE,
+                String.class,
+                String.class
+        };
+    }
+}
