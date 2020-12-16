d003f8d250729b0a9015939b73c3266eb9dabc0d
Drew Littler <drewlittler@hotmail.com>
2020-12-16 11:52:56 +0000

    Fix for SD-274

--- src/main/java/org/endeavourhealth/transform/common/resourceValidators/ResourceValidatorMedicationStatement.java
+++ src/main/java/org/endeavourhealth/transform/common/resourceValidators/ResourceValidatorMedicationStatement.java
@@ -1,7 +1,11 @@
 package org.endeavourhealth.transform.common.resourceValidators;
 
+import org.endeavourhealth.common.fhir.ExtensionConverter;
+import org.endeavourhealth.common.fhir.FhirExtensionUri;
+import org.endeavourhealth.transform.ui.helpers.ExtensionHelper;
 import org.hl7.fhir.instance.model.MedicationStatement;
 import org.hl7.fhir.instance.model.Resource;
+import org.hl7.fhir.instance.model.Type;
 
 import java.util.List;
 
@@ -14,5 +18,11 @@ public class ResourceValidatorMedicationStatement extends ResourceValidatorBase 
                 && medicationStatement.getDosage().size() > 1) {
             validationErrors.add("FHIR->Enterprise transform only supports MedicationStatements with a single dose");
         }
+
+        Type authType = ExtensionConverter.findExtensionValue(medicationStatement, FhirExtensionUri.MEDICATION_AUTHORISATION_TYPE);
+        if (authType == null) {
+            validationErrors.add("FHIR->Enterprise transform requires an authorisation type");
+        }
+
     }
 }
